2a9b04253c649ede5867398360345ca9e1741d39
vairocanakara <ltk118698@gmail.com>
2025-05-16 08:55:18 +0900

    minor frontend edit (no images yet)

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -1,6 +1,6 @@
-// apps/web/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 import Spaces from './pages/Spaces';
 import SpaceDetail from './pages/SpaceDetail';
 
@@ -8,7 +8,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Spaces} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/spaces" component={Spaces} />
         <Route path="/spaces/:id" component={SpaceDetail} />
       </Switch>
     </Router>
