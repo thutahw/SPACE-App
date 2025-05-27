7429f6dfda01a299ec8fd84c6b70a57bc14f2e7e
vairocanakara <ltk118698@gmail.com>
2025-05-27 22:57:29 +0900

    multer fix in package.json

--- apps/api/config/config.js
+++ apps/api/config/config.js
@@ -1,13 +1,31 @@
-require('dotenv').config(); // <-- loads from .env
+require('dotenv').config();
 
 module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
